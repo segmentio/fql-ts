6fa00bef0cb1767ac74337094b7721c63993feb8
Flaque <econrad@zagmail.gonzaga.edu>
2019-01-02 16:25:49 -0800

    Add base AST with tests

---
+++ src/ast.test.ts
@@ -0,0 +1,79 @@
+import ast, { AbstractSyntaxType } from './ast'
+import lex from './lexer'
+import { TokenType } from './token'
+
+test('root node has root type', () => {
+  const { tokens } = lex(`"foobang"`)
+  const { node } = ast(tokens)
+
+  expect(node.type).toBe(AbstractSyntaxType.ROOT)
+})
+
+test('second node has expr type', () => {
+  const { tokens } = lex(`"foobang"`)
+  const { node } = ast(tokens)
+
+  expect(node.nodes.length).toBe(1)
+  expect(node.nodes[0].type).toBe(AbstractSyntaxType.EXPR)
+})
+
+test('string types are recognized', () => {
+  const { tokens } = lex(`"foobang"`)
+  const { node } = ast(tokens)
+
+  expect(node.nodes[0].leaves.length).toBe(1)
+  expect(node.nodes[0].leaves[0].type).toBe(TokenType.String)
+})
+
+test('number types are recognized', () => {
+  const { tokens } = lex(`1`)
+  const { node } = ast(tokens)
+
+  expect(node.nodes[0].leaves.length).toBe(1)
+  expect(node.nodes[0].leaves[0].type).toBe(TokenType.Number)
+})
+
+test('null types are recognized', () => {
+  const { tokens } = lex(`null`)
+  const { node } = ast(tokens)
+
+  expect(node.nodes[0].leaves.length).toBe(1)
+  expect(node.nodes[0].leaves[0].type).toBe(TokenType.Null)
+})
+
+test('single paths are recognized', () => {
+  const { tokens } = lex(`message`)
+  const { node } = ast(tokens)
+
+  const path = node.nodes[0].nodes[0]
+
+  expect(path.type).toBe(AbstractSyntaxType.PATH)
+  expect(path.leaves[0].type).toBe(TokenType.Ident)
+  expect(path.leaves[0].value).toBe('message')
+})
+
+test('multi paths are recognized', () => {
+  const { tokens } = lex(`message.event.property`)
+  const { node } = ast(tokens)
+  const path = node.nodes[0].nodes[0]
+
+  expect(path.type).toBe(AbstractSyntaxType.PATH)
+
+  // message
+  expect(path.leaves[0].type).toBe(TokenType.Ident)
+  expect(path.leaves[0].value).toBe('message')
+
+  // .
+  expect(path.leaves[1].type).toBe(TokenType.Dot)
+
+  // event
+  expect(path.leaves[2].type).toBe(TokenType.Ident)
+  expect(path.leaves[2].value).toBe('event')
+
+  // .
+  expect(path.leaves[3].type).toBe(TokenType.Dot)
+
+  // property
+  expect(path.leaves[4].type).toBe(TokenType.Ident)
+  expect(path.leaves[4].value).toBe('property')
+})
---
+++ src/ast.ts
@@ -0,0 +1,145 @@
+import { Token, t, TokenType } from './token'
+
+export enum AbstractSyntaxType {
+  ROOT = 'root',
+  EXPR = 'expr',
+  PATH = 'path',
+  FUNC = 'func',
+  ERR = 'err'
+}
+
+// Splits children into "leaves" and "nodes" so we get
+// type safety and don't have to deal with union types
+export interface Node {
+  leaves: Token[]
+  nodes: Node[]
+  type: AbstractSyntaxType
+}
+
+function newNode(type: AbstractSyntaxType): Node {
+  return { nodes: [], leaves: [], type }
+}
+
+export class ParserError extends Error {
+  constructor(public message: string) {
+    super(message)
+    this.name = 'LexerError'
+    this.stack = (new Error() as any).stack
+  }
+}
+
+interface AstResponse {
+  node: Node
+  error?: Error
+}
+
+export default function ast(tokens: Token[]): AstResponse {
+  try {
+    return { node: new Parser(tokens).parse() }
+  } catch (error) {
+    return {
+      node: newNode(AbstractSyntaxType.ERR),
+      error
+    }
+  }
+}
+
+export class Parser {
+  private queue: Token[]
+
+  constructor(tokens: Token[]) {
+    this.queue = tokens
+  }
+
+  public parse(): Node {
+    const node: Node = newNode(AbstractSyntaxType.ROOT)
+
+    node.nodes.push(this.expr())
+    return node
+  }
+
+  private expr(): Node {
+    const node: Node = newNode(AbstractSyntaxType.EXPR)
+
+    const upcoming = this.peek()
+
+    // Strings
+    if (upcoming.type === TokenType.String) {
+      node.leaves.push(this.next())
+      return node
+    }
+
+    // Numbers
+    if (upcoming.type === TokenType.Number) {
+      node.leaves.push(this.next())
+      return node
+    }
+
+    // Null
+    if (upcoming.type === TokenType.Null) {
+      node.leaves.push(this.next())
+      return node
+    }
+
+    // Paths or functions
+    if (upcoming.type === TokenType.Ident) {
+      node.nodes.push(this.pathOrFunc(upcoming))
+      return node
+    }
+
+    // Lists
+    if (upcoming.type === TokenType.BrackLeft) {
+      node.nodes.push(this.list())
+      return node
+    }
+
+    // Unrecognized tokens
+    throw new ParserError(`Unsupported or unrecognized token: ${upcoming}`)
+  }
+
+  // things like `message.event` or `contains(...)`
+  private pathOrFunc(previous: Token): Node {
+    const id = this.next()
+
+    if (id.type === TokenType.ParenLeft) {
+      return this.func(previous)
+    }
+
+    return this.path(previous)
+  }
+
+  private path(previous: Token): Node {
+    const node = newNode(AbstractSyntaxType.PATH)
+    node.leaves.push(previous)
+
+    while (this.peek().type === TokenType.Ident || this.peek().type === TokenType.Dot) {
+      node.leaves.push(this.next())
+    }
+
+    return node
+  }
+
+  private func(previous: Token): Node {
+    throw new ParserError(`${previous} func Not yet supported`)
+  }
+
+  private list(): Node {
+    throw new ParserError(` list Not yet supported`)
+  }
+
+  private peek(): Token {
+    if (this.queue.length > 0) {
+      return this.queue[0]
+    }
+
+    return t.EOS()
+  }
+
+  private next(): Token {
+    const upcoming = this.peek()
+    if (upcoming.type !== TokenType.EOS) {
+      return this.queue.shift()
+    }
+    return t.EOS()
+  }
+}
