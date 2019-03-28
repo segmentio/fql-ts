0cc562185b9337e814deda5062e36f8725d7bc64
Jason Tu <jasontu4@gmail.com>
2019-03-28 11:52:25 -0700

    fix lint

--- src/ast.test.ts
+++ src/ast.test.ts
@@ -262,12 +262,14 @@ test('We can convert conditional statements from ast nodes back to strings', () 
   expect(astToString(node)).toBe('type = "track" or type = "identify"')
 })
 
-;[
+const ConditionalStatementTests = [
   `contains("Evan Conrad", "Evan") and event = "foo"`,
   `contains("Evan Conrad", "Evan") or event = "foo"`,
   `event = "foo" and contains("Evan Conrad", "Evan")`,
   `event = "foo" or contains("Evan Conrad", "Evan")`,
-].forEach(fql => {
+]
+
+ConditionalStatementTests.forEach(fql => {
   test(`conditional statement with function: ${fql}`, () => {
     const { tokens, error } = lex(fql)
     expect(error).toBeUndefined()
